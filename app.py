121b6e37bc5de33ec405cf50a918a7e025001bf3
shirley Yang <yangyudi19880609@126.com>
2022-06-11 03:20:02 +0800

    Update

--- app.py
+++ app.py
@@ -39,10 +39,17 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     mag = event.message.text
-    s = '你吃飯了嗎'
+    r = '很抱歉，我不太懂您的意思'#預設回傳訊息
+
+
+    if msg == 'hi':
+        r = 'hi~'
+    elif mag == '你吃飯了嗎':
+        r = '還沒'
+
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text = s))
+        TextSendMessage(text = r))
 
 
 if __name__ == "__main__":
